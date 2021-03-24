c9e92aac89fa0d1bad2fc5ee0eb5a56a3388a417
mariondechallens <35600270+mariondechallens@users.noreply.github.com>
2021-03-24 11:20:39 +0100

    Add all_checks.py

---
+++ all_checks.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Coursera : Git and GitHub Course - Week 1
+"""
+import os
+import sys
+
+def check_reboot():
+    """Returns true if the computer has appending reboot."""
+    
+    return os.path.exists("/run/reboot-required")
+    
+def main():
+    if check_reboot():
+        print("Pending reboot.")
+        sys.exit(1)
+        
+    print('Everything ok.')
+    sys.exit(0)
+
+main()
